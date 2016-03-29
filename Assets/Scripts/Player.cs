d7d14383c14978315828042ab2d7501e323bc282
Robin Lloyd <angelssindesign@gmail.com>
2016-03-29 15:39:42 +0100

    intial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D _myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		_myRigidbody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveH = Input.GetAxis("Horizontal");
+		float moveV = Input.GetAxis("Vertical");
+		_myRigidbody.velocity = new Vector2(moveH,  moveV);
+	}
+}
